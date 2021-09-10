153801ae47cdb0f1bd7c630bf186fb8a3cb5f4ea
Miguel Angel Mauricio Torrejon <82061770+Xharu@users.noreply.github.com>
2021-09-10 08:53:55 -0400

    Add files via upload

---
+++ Sejemplo01/Pejemplo01/principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "mi nombre es: Miguel Angel Mauricio Torrejon" << endl;
+	cout << "mi cedula de identidad es: 10387886" << endl;
+		return 0;
+}
